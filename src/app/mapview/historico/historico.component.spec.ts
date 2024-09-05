b4442ebc777d7bbe4d17dbf0ab4ea083092d6348
NaaQuinteiro <thaynaquinteiro@gmail.com>
2024-09-05 16:40:33 -0300

    Page histórico

--- src/app/mapview/historico/historico.component.spec.ts
+++ src/app/mapview/historico/historico.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HistoricoComponent } from './historico.component';
+import { MatCommonModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('HistoricoComponent', () => {
   let component: HistoricoComponent;
@@ -8,7 +13,14 @@ describe('HistoricoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HistoricoComponent]
+      imports: [
+        HistoricoComponent,
+        MatCommonModule,
+        MatButtonModule,
+        MatMenuModule,
+        CommonModule,
+        MatIconModule
+      ]
     })
     .compileComponents();
     
