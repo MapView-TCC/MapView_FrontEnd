4b3baac044c19498a3402608600ac2c2eb50dfc2
NaaQuinteiro <thaynaquinteiro@gmail.com>
2024-09-10 09:42:10 -0300

    Calendario semi consluido

---
+++ src/app/mapview/calendario/calendario.component.ts
@@ -0,0 +1,67 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
+
+@Component({
+  selector: 'app-calendario',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calendario.component.html',
+  styleUrl: './calendario.component.scss'
+})
+export class CalendarioComponent implements OnInit{
+  currentMonth: moment.Moment;
+  days: moment.Moment[];
+  selectedDate: moment.Moment | null = null;
+
+  constructor(){
+    this.currentMonth = moment();
+    this.days = [];
+  }
+
+  ngOnInit(): void {
+    this.generateCalendar();
+  }
+
+  generateCalendar(): void {
+
+    // Use o método startOf corretamente
+    let startOfMonth = this.currentMonth.clone().startOf('month').startOf('week');
+    const endOfMonth = this.currentMonth.clone().endOf('month').endOf('week')
+    
+    let day = startOfMonth.clone();
+    this.days = [];
+
+    while (day.isBefore(endOfMonth, 'day')){
+      this.days.push(day.clone());
+      day.add(1, 'day');
+    }
+  }
+
+  isToday(day: moment.Moment): boolean {
+    return day.isSame(moment(), 'day');
+  }
+
+  isSelected(day: moment.Moment): boolean {
+    return this.selectedDate ? day.isSame(this.selectedDate, 'day') : false;
+  }
+
+  selectDate(day: moment.Moment): void {
+    this.selectedDate = day;
+  }
+
+  changeMonth(offset: number): void {
+    this.currentMonth.add(offset, 'month');
+    this.generateCalendar();
+  }
+
+  // submitDate(): void {
+  //   if (this.selectedDate) {
+  //     // Aqui você pode enviar a data selecionada para o backend
+  //     console.log('Data selecionada:', this.selectedDate.format('YYYY-MM-DD'));
+  //     // Por exemplo, usando HttpClient:// 
+  //     this.http.post('sua-url-para-backend', { date: this.selectedDate.format('YYYY-MM-DD') })//   
+  //     .subscribe(response => console.log(response));
+  //   }
+  // }
+}
--- src/app/mapview/historico/historico.component.ts
+++ src/app/mapview/historico/historico.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NotificationCardComponent } from '../notification-card/notification-card.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,6 +12,8 @@ import {AsyncPipe} from '@angular/common';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { CalendarioComponent } from "../calendario/calendario.component";
+
 
 export interface StateGroup {
   letter: string;
@@ -28,7 +30,7 @@ export const _filter = (opt: string[], value: string): string[] => {
   selector: 'app-historico',
   standalone: true,
   imports: [
-    HeaderComponent, 
+    HeaderComponent,
     NotificationCardComponent,
     CommonModule,
     MatIconModule,
@@ -38,7 +40,8 @@ export const _filter = (opt: string[], value: string): string[] => {
     MatInputModule,
     MatAutocompleteModule,
     AsyncPipe,
-  ],
+    CalendarioComponent
+],
   templateUrl: './historico.component.html',
   styleUrl: './historico.component.scss'
 })
@@ -164,4 +167,5 @@ export class HistoricoComponent implements OnInit {
   
       return this.stateGroups;
     }
+    
   }
