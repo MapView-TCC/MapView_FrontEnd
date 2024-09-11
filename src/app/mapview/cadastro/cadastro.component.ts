0766716101ec8e9b583cb4a5eefd60a298ac5110
NaaQuinteiro <thaynaquinteiro@gmail.com>
2024-09-11 13:06:09 -0300

    Inicio tela de cadastro

--- src/app/mapview/cadastro/cadastro.component.ts
+++ src/app/mapview/cadastro/cadastro.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import {Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms'; // Criar formulários reativos
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    MatIconModule, 
+    FooterComponent
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
 
+  cadastroEquipamento!: FormGroup
+
+  constructor(){
+    this.cadastroEquipamento = new FormGroup({
+
+    })
+  }
+  
 }
+
