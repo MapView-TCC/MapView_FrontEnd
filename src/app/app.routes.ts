f0121bccf9cd2551b987a106f0cbc9b2c0417c13
joaovictor001 <joaocarvalhoborges@outlook.com>
2024-09-27 13:27:12 -0300

    :sparkles: feat: alteraçoes para SSO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
 
     {path:'', component:InicialComponent},
     {path:'header' , component:HeaderComponent},
-    {path:'ambiente',component:AmbienteComponent},
+    {path:'callback',component:AmbienteComponent},
     {path:'cadastro', component: CadastroComponent},
     {path:'historico', component:HistoricoComponent},
     {path:'iventario',component:IventarioComponent}
