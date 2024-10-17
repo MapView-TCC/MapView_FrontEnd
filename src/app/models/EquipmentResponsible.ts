f5dbe9730399eccff47fb627db388fe5fe1390ea
brunannunes <brunanunes324@gmail.com>
2024-10-17 14:36:18 -0300

    arrumando umas coisinhas

---
+++ src/app/models/EquipmentResponsible.ts
@@ -0,0 +1,8 @@
+import { Equipment } from "./Equipment";
+import { Responsible } from "./Responsible";
+
+export class EquipmentResponsible{
+    id_equip_rep: number=0;
+    equipment: Equipment = new Equipment();
+    responsible: Responsible = new Responsible();
+}
---
+++ src/app/models/Responsible.ts
@@ -0,0 +1,8 @@
+
+
+export class Responsible{
+    responsible_name: string = '';
+    edv: string = '';
+    id_classes: number=1;
+    id_user: number=1;
+}
