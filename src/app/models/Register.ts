dd4708d2816aa7c5fce303061a4132f384189c8d
Anasutero <anamaria.sutero.10@gmail.com>
2024-10-28 16:25:09 -0300

    put adionado na tela de invenatrio , esta dando erro 409

--- src/app/models/Register.ts
+++ src/app/models/Register.ts
@@ -16,3 +16,19 @@ export class Register{
     costCenter_name: string = '';
     dataResponsible: DataResponsible[] = [];
 }
+
+export class RegisterUpdate{
+    id_equipment: string = '';
+    name_equipment: string = '';
+    rfid: number = 0;
+    type: string = '';
+    model: string = '';
+    validity: string = '';
+    admin_rights: string = '';
+    observation: string = '';
+    id_environment: number = 0;
+    post: string = '';
+    id_owner: string = '';
+    costCenter_name: string = '';
+    dataResponsible: DataResponsible[] = [];
+}
