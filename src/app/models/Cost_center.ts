094766c85dffe47e7c73350b54c4eb26da26d11a
NaaQuinteiro <thaynaquinteiro@gmail.com>
2024-11-01 13:37:42 -0300

    📂 models/✅Cost_center

--- src/app/models/Cost_center.ts
+++ src/app/models/Cost_center.ts
@@ -1,11 +1,10 @@
 export class Cost_center{
     id_cost_center: number = 0;
-    cost_center_name: string = "";
-    operative: boolean = true;
+    costCenter: string = "";
 }
 
-export class Cost_centerIventario{
-    id_cost_center: number = 0;
-    constcenter: string = "";
-    operative: boolean = true;
-}
+// export class Cost_centerIventario{
+//     id_cost_center: number = 0;
+//     constcenter: string = "";
+//     operative: boolean = true;
+// }
